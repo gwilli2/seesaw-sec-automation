de72023ea2a0bcfbec3aab572378649cdb739b13
Gregory Williams II <greglwilliamsii@gmail.com>
2026-04-27 19:39:55 -0400

    Add listeners for github events.

--- infra/main.tf
+++ infra/main.tf
@@ -32,3 +32,34 @@ resource "aws_iam_role_policy" "guardian_policy" {
     }]
   })
 }
+
+# 1. Create a "Sensor" that listens for S3 configuration changes
+resource "aws_cloudwatch_event_rule" "s3_change_sensor" {
+  name        = "s3-change-sensor"
+  description = "Triggers when an S3 bucket's public access settings change"
+
+  event_pattern = jsonencode({
+    source      = ["aws.s3"]
+    detail_type = ["AWS API Call via CloudTrail"]
+    detail = {
+      eventSource = ["s3.amazonaws.com"]
+      eventName   = ["PutBucketPublicAccessBlock"]
+    }
+  })
+}
+
+# 2. Connect the Sensor to your Python Lambda
+resource "aws_cloudwatch_event_target" "trigger_lambda" {
+  rule      = aws_cloudwatch_event_rule.s3_change_sensor.name
+  target_id = "S3Guardian"
+  arn       = aws_lambda_function.s3_guardian.arn # Ensure this name matches your lambda resource name
+}
+
+# 3. Give the Sensor permission to "invoke" (call) the Lambda
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.s3_guardian.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.s3_change_sensor.arn
+}
